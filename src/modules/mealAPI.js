a2ce19d516b919bcd3c8b71753d168d31b85e735
Andres Agudelo <anagudelogu@gmail.com>
2022-05-16 22:21:58 -0500

    Add getByCategory method which fetchs all recipes from a given category

--- src/modules/mealAPI.js
+++ src/modules/mealAPI.js
@@ -4,6 +4,7 @@ const urlMap = {
   random: 'https://www.themealdb.com/api/json/v1/1/random.php',
   categories:
     'https://www.themealdb.com/api/json/v1/1/categories.php',
+  byCategory: 'https://www.themealdb.com/api/json/v1/1/filter.php?c=',
 };
 
 export default class MealAPI {
@@ -11,5 +12,19 @@ export default class MealAPI {
     const fetchRequest = new FetchRequest({ url: urlMap.random });
     const data = await fetchRequest.call();
     return data.meals[0];
+  }
+
+  static async getCategories() {
+    const fetchRequest = new FetchRequest({ url: urlMap.categories });
+    const categories = await fetchRequest.call();
+    return categories.categories;
+  }
+
+  static async getByCategory(category) {
+    const fetchRequest = new FetchRequest({
+      url: `${urlMap.byCategory}${category}`,
+    });
+    const recipes = await fetchRequest.call();
+    return recipes.meals;
   }
 }
