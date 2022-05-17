4e27351a734d52004cd2f0d58c420e14c658dea8
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-17 11:23:08 -0500

    Refactor card templates

--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -15,17 +15,47 @@ export default class UserInterface {
   static createRecipeCard({ strMeal = '', strMealThumb = '' }) {
     const LI = document.createElement('li');
     LI.classList.add('recipes__card');
-    LI.innerHTML = `
-    <img class="recipes__image" src="${strMealThumb}" alt="Delicious ${strMeal}">
-    <div class="recipes__content">
-        <div class="recipes__header">
-            <h2 class="recipes__title">${strMeal}</h2>
-            <i class="fa-regular fa-heart"></i>
-        </div>
-        <span class="recipes__likes">N likes</span>
-        <button class="recipes__comments">Comments</button>
-        <button class="recipes__reservations">Reservations</button>
-    </div>`;
+
+    const img = document.createElement('img');
+    img.src = strMealThumb;
+    img.classList.add('recipes__image');
+    img.setAttribute('alt', `Delicious ${strMeal}`);
+
+    const content = document.createElement('div');
+    content.classList.add('recipes__content');
+
+    const header = document.createElement('div');
+    header.classList.add('recipes__header');
+
+    const title = document.createElement('h2');
+    title.innerHTML = strMeal;
+    title.classList.add('recipes__title');
+
+    const likeBtn = document.createElement('i');
+    likeBtn.setAttribute('class', 'fa-regular fa-heart');
+
+    header.appendChild(title);
+    header.appendChild(likeBtn);
+
+    const likes = document.createElement('span');
+    likes.classList.add('recipes__likes');
+    likes.innerHTML = 'N likes';
+
+    const comments = document.createElement('button');
+    comments.classList.add('recipes__comments');
+    comments.innerHTML = 'Comments';
+
+    const reservations = document.createElement('button');
+    reservations.classList.add('recipes__reservations');
+    reservations.innerHTML = 'Reservations';
+
+    content.appendChild(header);
+    content.appendChild(likes);
+    content.appendChild(comments);
+    content.appendChild(reservations);
+
+    LI.appendChild(img);
+    LI.appendChild(content);
 
     recipeList.appendChild(LI);
   }
