a67e36db93bf671a69e883b4fca136fb9fc4f67d
Andres Agudelo <anagudelogu@gmail.com>
2022-05-16 21:43:06 -0500

    Add userInterface class to handle DOM interactions

---
+++ src/modules/userInterface.js
@@ -0,0 +1,28 @@
+import MealAPI from './mealAPI';
+
+const recipeList = document.querySelector('.recipes__list');
+export default class UserInterface {
+  static async displayRecipes() {
+    for (let i = 0; i < 9; i++) {
+      const randomRecipe = await MealAPI.getRandomRecipe();
+      UserInterface.createRecipeCard(randomRecipe);
+    }
+  }
+
+  static createRecipeCard({ strMeal, strMealThumb }) {
+    const LI = document.createElement('li');
+    LI.classList.add('recipes__card');
+    LI.innerHTML = `<img class="recipes__image" src="${strMealThumb}" alt="Delicious ${strMeal}">
+    <div class="recipes__content">
+        <div class="recipes__header">
+            <h2 class="recipes__title">${strMeal}</h2>
+            <i class="fa-regular fa-heart"></i>
+        </div>
+        <span class="recipes__likes">N likes</span>
+        <button class="recipes__comments">Comments</button>
+        <button class="recipes__reservations">Reservations</button>
+    </div>`;
+
+    recipeList.appendChild(LI);
+  }
+}
