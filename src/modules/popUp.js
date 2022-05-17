5c2f09017fabd20b3384fcb45d1df151da6313e8
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-17 16:35:02 -0500

    Add popup
    Add popup on comments.
    
    Add comments.
    
    Add input for comments.
    
    Add popup on reviews.

---
+++ src/modules/popUp.js
@@ -0,0 +1,64 @@
+export default class PopUp {
+  static pop(data) {
+    this.data = data;
+    this.template(this.data.type);
+  }
+
+  static template(type) {
+    const container = document.createElement('div');
+    container.setAttribute('id', this.data.idMeal);
+    container.setAttribute('class', 'recipes__popup');
+
+    const commentSection = PopUp.commentsTemplate(this.data.type);
+    const reviewSection = PopUp.reviewsTemplate(this.data.type);
+
+    container.innerHTML = `
+      <img class="recipes__popup_image" src="${this.data.strMealThumb}" alt="Delicious ${this.data.strMeal}">
+      <i class="fa-solid fa-xmark"></i>
+      <h2>${this.data.strMeal}</h2>
+      
+      ${commentSection}
+      ${reviewSection}
+    `;
+    container.querySelector('i').addEventListener('click', () => {
+      container.remove();
+    });
+    document.body.appendChild(container);
+  }
+
+  static commentsTemplate(type) {
+    if (type !== 'Recipe') return '';
+    let comments = '';
+    this.data.comments.forEach((comment) => {
+      comments += `
+        <li class="recipes__popup_comment">
+          <span class="recipes__popup_comment-user">
+            ${comment.username}:
+          </span>
+          <span class="recipes__popup_comment-content">
+            ${comment.comment}
+          </span>
+          <span class="recipes__popup_comment-date">
+            ${comment.creation_date}
+          </span>
+        </li>
+      `;
+    });
+    return `
+      <div>
+        <span>Comments (${this.data.comments.length})</span>
+        <ul class="recipes__popup_comments">${comments}</ul>
+        <input class="recipes__popup_input-user" placeholder="User" tabindex=0></input>
+        <input  class="recipes__popup_input-comment" placeholder="Comment" tabindex=0></input>
+        <button class="recipes__popup_input-submit" type="button" tabindex=0>Comment</button>
+      </div>
+    `;
+  }
+
+  static reviewsTemplate(type) {
+    if (type !== 'Category') return '';
+    return '';
+  }
+
+  static submitComment() {}
+} 
--- src/modules/userInterface.js
+++ src/modules/userInterface.js
@@ -1,61 +1,57 @@
 import MealAPI from './mealAPI.js';
+import PopUp from './popUp.js';
 
 const recipeList = document.querySelector('.recipes__list');
 
 export default class UserInterface {
-  static async displayRecipes() {
+  constructor() {
+    this.recipes = {};
+    this.categories = {};
+  }
+
+  static async displayRecipes(category = 'Pasta') {
     try {
-      const pasta = await MealAPI.getByCategory('Pasta');
-      pasta.forEach((recipe) => this.createRecipeCard(recipe));
+      const pasta = await MealAPI.getByCategory(category);
+      pasta.forEach((recipe) => this.createCard(recipe));
     } catch (error) {
       throw new Error(error);
     }
   }
 
-  static createRecipeCard({ strMeal = '', strMealThumb = '' }) {
+  static async displayCategories() {
+    try {
+      const pasta = await MealAPI.getCategories();
+      pasta.forEach((category) => this.createCard(category));
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
+  static createCard({ strMeal = '', strMealThumb = '', idMeal, likes = 1}) {
     const LI = document.createElement('li');
     LI.classList.add('recipes__card');
+    LI.innerHTML = `
+      <img class="recipes__image" src="${strMealThumb}" alt="Delicious ${strMeal}">
+      <div class="recipes__content">
+          <div class="recipes__header">
+              <h2 class="recipes__title">${strMeal}</h2>
+              <i class="fa-regular fa-heart"></i>
+          </div>
+          <span class="recipes__likes">${likes} like${likes === 1 ? '': 's'}</span>
+          <button class="recipes__comments">Comments</button>
+          <button class="recipes__reservations">Reservations</button>
+      </div>
+    `;
 
-    const img = document.createElement('img');
-    img.src = strMealThumb;
-    img.classList.add('recipes__image');
-    img.setAttribute('alt', `Delicious ${strMeal}`);
-
-    const content = document.createElement('div');
-    content.classList.add('recipes__content');
-
-    const header = document.createElement('div');
-    header.classList.add('recipes__header');
-
-    const title = document.createElement('h2');
-    title.innerHTML = strMeal;
-    title.classList.add('recipes__title');
-
-    const likeBtn = document.createElement('i');
-    likeBtn.setAttribute('class', 'fa-regular fa-heart');
-
-    header.appendChild(title);
-    header.appendChild(likeBtn);
-
-    const likes = document.createElement('span');
-    likes.classList.add('recipes__likes');
-    likes.innerHTML = 'N likes';
-
-    const comments = document.createElement('button');
-    comments.classList.add('recipes__comments');
-    comments.innerHTML = 'Comments';
-
-    const reservations = document.createElement('button');
-    reservations.classList.add('recipes__reservations');
-    reservations.innerHTML = 'Reservations';
-
-    content.appendChild(header);
-    content.appendChild(likes);
-    content.appendChild(comments);
-    content.appendChild(reservations);
-
-    LI.appendChild(img);
-    LI.appendChild(content);
+    const openComments = LI.querySelector('.recipes__comments');
+    openComments.addEventListener('click', () => {
+      const comments = [
+        {username: 'Daniel', comment: 'I like this one.', 'creation_date': 'May 5th'},
+        {username: 'Andrés', comment: '5/10', 'creation_date': 'Feb 19th'},
+        {username: 'Joseph', comment: 'AMAZING DISH!!', 'creation_date': 'Dec 1st'}
+      ];
+      PopUp.pop({strMeal, strMealThumb, idMeal, comments, type: 'Recipe'})
+    });
 
     recipeList.appendChild(LI);
   }
