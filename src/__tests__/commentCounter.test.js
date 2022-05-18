6f9d4a381b8a96aa625168c54df205dd681da398
Daniel Malo <danielmaloosorio@gmail.com>
2022-05-18 14:15:55 -0500

    Count comment number tests
    Count 0 comments.
    
    Count add 1 comment.
    
    Count add n comments.

--- src/__tests__/commentCounter.test.js
+++ src/__tests__/commentCounter.test.js
@@ -1,7 +1,39 @@
 import PopUp from "../modules/popUp";
 
+const createEnv = (comments = []) => {
+  PopUp.pop({
+    strMeal: 'meal', strMealThumb: 'thumb', idMeal: 0, comments: comments, type: 'Recipe',
+  });
+  const container = document.createElement('div');
+  const el = document.createElement('span');
+  el.classList.add('recipes__popup_comment-count');
+  container.appendChild(el);
+  return [container];
+}
+
 describe('Counting number of comments', () => {
-  it('Comments', () => {
+  it('Count 0 comments', () => {
+    createEnv();
+    const num = PopUp.data.comments.length;
+
+    expect(num).toEqual(0);
+  });
+
+  it('Add 1 comment', () => {
+    const [container1] = createEnv();
+    const num = PopUp.commentCountAdd(container1);
+
+    expect(num).toEqual(1);
+  });
+
+  it('Add \'n\' comments', (n = 8) => {
+    const [container2] = createEnv([]);
+    let num = 0;
+    for (let i = 0; i < n; i += 1) {
+      num = PopUp.commentCountAdd(container2);
+    }
     
+
+    expect(num).toEqual(n);
   });
 });
